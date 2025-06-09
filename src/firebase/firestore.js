96f1417b38c514bfad0f9def3144cf18a63f156c
coderushvic <coderushdevvic@gmail.com>
2025-06-09 10:45:07 +0100

    Update firestore.js

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -5,12 +5,13 @@ import { getFirestore, collection, getDocs, query, where, updateDoc, doc } from 
 
 // Add the Web App's Firebase configuration.
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY, // Ensure this is set correctly
+  apiKey: process.env.REACT_APP_API_KEY, // Ensure this is set correctly
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID, // Make sure this is not undefined
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase only if it hasn't been initialized already
